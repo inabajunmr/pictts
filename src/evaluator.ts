013b1d845f573007ed7ab5c67ae79fef659e84b0
inabajunmr <inabajun.for.regi@gmail.com>
2020-12-25 20:34:13 +0900

    wip

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -4,5 +4,46 @@ export class Pict {
         this.parameters = parameters;
     }
 
-    // TODO implement this
+    private allSlotCombination(factorCount: number) {
+        const keyCombinations = this.allFactorCombination(factorCount);
+        return;
+    }
+
+    private allFactorCombination(factorCount: number): string[][] {
+        const parray = new Array<string>();
+        for (const key in this.parameters.keys) {
+            parray.push(key);
+        }
+        return this.bind(parray, factorCount);
+    }
+
+    private bind(input: string[], fcount: number): string[][] {
+        const result = new Array<Array<string>>();
+        this.ibind(input, [], fcount, 0, result);
+        return result;
+    }
+
+    private ibind(
+        input: string[],
+        tmp: string[],
+        fcount: number,
+        depth: number,
+        a: string[][]
+    ) {
+        input.forEach((v, i) => {
+            tmp[depth] = v;
+            if (depth === fcount - 1) {
+                a.push(Array.from(tmp));
+                return;
+            }
+
+            return this.ibind(
+                input.slice(i + 1, input.length),
+                tmp,
+                fcount,
+                depth + 1,
+                a
+            );
+        });
+    }
 }
