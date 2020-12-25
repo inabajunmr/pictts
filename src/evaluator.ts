47516a624793de7ec4270aebe112a23aaa1ee3fe
inabajunmr <inabajun.for.regi@gmail.com>
2020-12-26 00:21:41 +0900

    wip

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -6,7 +6,38 @@ export class Pict {
 
     private allSlotCombination(factorCount: number) {
         const keyCombinations = this.allFactorCombination(factorCount);
+        keyCombinations.map((c) => this.allSlot(c)); // TODO
+        // キーの組み合わせに対して全パターンの組み合わせを作る
+        // keyCはa,b,cで2であれば[a,b][a,c][b,c]
+        // a=[a1,a2] b=[b1,b2] c=[c1,c2]であれば[a,b]=[[a1,b1],[a1,b2],[a2,b2]]
+
         return;
+    }
+
+    private allSlot(keys: string[]) {
+        const result = new Array<Array<string>>();
+
+        const a = keys.map((k) => {
+            return this.parameters.get(k) as string[];
+        });
+        this.iAllSlot(a, 0, [], result);
+    }
+
+    private iAllSlot(
+        // TODO test
+        parameters: string[][],
+        depth: number,
+        tmp: string[],
+        result: string[][]
+    ) {
+        if (depth == parameters.length) {
+            result.push(tmp);
+        }
+
+        parameters[depth].forEach((p) => {
+            tmp.push(p);
+            this.iAllSlot(parameters, depth + 1, tmp, result);
+        });
     }
 
     private allFactorCombination(factorCount: number): string[][] {
