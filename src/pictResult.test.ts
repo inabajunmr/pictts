1987676e8a029d3cead2bb5e1f0658c265437c3b
inabajunmr <inabajun.for.regi@gmail.com>
2021-01-01 18:53:18 +0900

    wip

--- src/pictResult.test.ts
+++ src/pictResult.test.ts
@@ -54,6 +54,65 @@ test('put', () => {
     expect(sut.nowKey().length).toBe(0);
 });
 
+test('revert', () => {
+    const sut = new PictResult([Key.of('A'), Key.of('B'), Key.of('C')]);
+
+    sut.put(map('A', 'A1'));
+    sut.put(map('B', 'B1'));
+    sut.put(map('C', 'C1'));
+    sut.put(map('A', 'A2'));
+    sut.put(map('B', 'B2'));
+    sut.put(map('C', 'C2'));
+
+    expect(sut.result.length).toBe(2);
+    expect(
+        assertEquals(map3('A', 'A2', 'B', 'B2', 'C', 'C2'), sut.result[1])
+    ).toBe(true);
+    expect(sut.contains(map('C', 'C2'))).toBe(true);
+    expect(sut.nowIsFull()).toBe(true);
+    expect(sut.nowKey().length).toBe(0);
+
+    sut.revert();
+    expect(sut.result.length).toBe(2);
+    expect(assertEquals(map2('A', 'A2', 'B', 'B2'), sut.result[1])).toBe(true);
+    expect(sut.contains(map('B', 'B2'))).toBe(true);
+    expect(sut.nowIsFull()).toBe(false);
+    expect(sut.nowKey()[0]).toBe(Key.of('A'));
+    expect(sut.nowKey()[1]).toBe(Key.of('B'));
+
+    sut.revert();
+    expect(sut.result.length).toBe(2);
+    expect(assertEquals(map('A', 'A2'), sut.result[1])).toBe(true);
+    expect(sut.contains(map('A', 'A2'))).toBe(true);
+    expect(sut.nowIsFull()).toBe(false);
+    expect(sut.nowKey()[0]).toBe(Key.of('A'));
+
+    sut.revert();
+    expect(sut.result.length).toBe(1);
+    expect(
+        assertEquals(map3('A', 'A1', 'B', 'B1', 'C', 'C1'), sut.result[0])
+    ).toBe(true);
+    expect(sut.contains(map('C', 'C1'))).toBe(true);
+    expect(sut.nowIsFull()).toBe(true);
+    expect(sut.nowKey().length).toBe(0);
+
+    sut.revert();
+    expect(sut.result.length).toBe(1);
+    expect(assertEquals(map2('A', 'A1', 'B', 'B1'), sut.result[0])).toBe(true);
+    expect(sut.contains(map('B', 'B1'))).toBe(true);
+    expect(sut.nowIsFull()).toBe(false);
+    expect(sut.nowKey()[0]).toBe(Key.of('A'));
+    expect(sut.nowKey()[1]).toBe(Key.of('B'));
+
+    sut.revert();
+    expect(sut.result.length).toBe(1);
+    expect(assertEquals(map('A', 'A1'), sut.result[0])).toBe(true);
+    expect(sut.contains(map('A', 'A1'))).toBe(true);
+    expect(sut.contains(map('A', 'A2'))).toBe(false);
+    expect(sut.nowIsFull()).toBe(false);
+    expect(sut.nowKey()[0]).toBe(Key.of('A'));
+});
+
 function assertEquals(
     target: Map<Key, Value>,
     result: Map<Key, Value>
