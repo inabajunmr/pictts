dcb6da2cf29468d748d9b0b7b828bed615cdcc4e
inabajunmr <inabajun.for.regi@gmail.com>
2021-01-03 02:23:33 +0900

    add sample values for ui

--- pages/src/js/app.ts
+++ pages/src/js/app.ts
@@ -5,6 +5,19 @@ export function pict(input:string):P.PictResult {
     return pict.testCases();
 }
 
+const sample = `Type:           Primary, Logical, Single, Span, Stripe, Mirror, RAID-5
+Size:           10, 100, 500, 1000, 5000, 10000, 40000
+Format method:  quick, slow
+File system:    FAT, FAT32, NTFS
+Cluster size:   512, 1024, 2048, 4096, 8192, 16384, 32768, 65536
+Compression:    on, off
+
+IF [File system] = "FAT"   THEN [Size] <= 4096 ;
+IF [File system] = "FAT32" THEN [Size] <= 32000 ;`
+
+document.getElementById("input")!.textContent = sample;
+document.getElementById("result")!.textContent = pict((document.getElementById("input") as any).value).toString();
+
 document.getElementById("input")!.addEventListener("input", () => {
-    document.getElementById("result")!.textContent = pict((document.getElementById("input") as any).value).toString()
+    document.getElementById("result")!.textContent = pict((document.getElementById("input") as any).value).toString();
 })    
